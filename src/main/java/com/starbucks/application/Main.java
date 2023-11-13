e4180f0c58408be5148068a34c9cb20fa820d834
Supriyakankati <120232768+Supriyakankati@users.noreply.github.com>
2023-11-14 00:17:23 +0200

    Add main view with staff and customer role selection

--- src/main/java/com/starbucks/application/Main.java
+++ src/main/java/com/starbucks/application/Main.java
@@ -1,24 +1,41 @@
 package com.starbucks.application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Starbucks Application");
-        
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+	private Stage primaryStage;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		primaryStage.setTitle("Starbucks Application");
+
+		showMainView();
+	}
+
+	private void showMainView() {
+		Button staffButton = new Button("STAFF");
+		staffButton.setPrefSize(150, 50);
+
+		Button customerButton = new Button("CUSTOMER");
+		customerButton.setPrefSize(150, 50);
+
+		HBox hBox = new HBox(10);
+		hBox.setAlignment(Pos.CENTER);
+		hBox.getChildren().addAll(staffButton, customerButton);
+
+		Scene scene = new Scene(hBox, 600, 400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
